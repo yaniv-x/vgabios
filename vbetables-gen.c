23e5628367c6867eed4415c93abbd4f45692813a
Yaniv Kamay <yaniv@objectivex.org>
2010-11-14 05:51:31 +0200

    cleanup

--- vbetables-gen.c
+++ vbetables-gen.c
@@ -93,154 +93,151 @@ int main(int argc, char **argv)
     else
       pitch = pm->width * ((pm->depth + 7) / 8);
 
-      printf("{ 0x%04x, /* %dx%dx%d */\n", 
-             pm->mode, pm->width, pm->height, pm->depth);
-      if (pm->depth == 4)
-        printf("{ /*Bit16u ModeAttributes*/ %s,\n", 
-               "VBE_MODE_ATTRIBUTE_SUPPORTED | "
-               "VBE_MODE_ATTRIBUTE_EXTENDED_INFORMATION_AVAILABLE | "
-               "VBE_MODE_ATTRIBUTE_COLOR_MODE | "
-               "VBE_MODE_ATTRIBUTE_TTY_BIOS_SUPPORT | "
-               "VBE_MODE_ATTRIBUTE_GRAPHICS_MODE");
-      else
-        printf("{ /*Bit16u ModeAttributes*/ %s,\n", 
-               "VBE_MODE_ATTRIBUTE_SUPPORTED | "
-               "VBE_MODE_ATTRIBUTE_EXTENDED_INFORMATION_AVAILABLE | "
-               "VBE_MODE_ATTRIBUTE_COLOR_MODE | "
-               "VBE_MODE_ATTRIBUTE_LINEAR_FRAME_BUFFER_MODE | "
-               "VBE_MODE_ATTRIBUTE_GRAPHICS_MODE");
-      printf("/*Bit8u  WinAAttributes*/ %s,\n",
-             "VBE_WINDOW_ATTRIBUTE_RELOCATABLE | "
-             "VBE_WINDOW_ATTRIBUTE_READABLE | "
-             "VBE_WINDOW_ATTRIBUTE_WRITEABLE");
+    printf("{ 0x%04x, /* %dx%dx%d */\n", 
+           pm->mode, pm->width, pm->height, pm->depth);
+    if (pm->depth == 4)
+      printf("{ /*Bit16u ModeAttributes*/ %s,\n", 
+             "VBE_MODE_ATTRIBUTE_SUPPORTED | "
+             "VBE_MODE_ATTRIBUTE_EXTENDED_INFORMATION_AVAILABLE | "
+             "VBE_MODE_ATTRIBUTE_COLOR_MODE | "
+             "VBE_MODE_ATTRIBUTE_TTY_BIOS_SUPPORT | "
+             "VBE_MODE_ATTRIBUTE_GRAPHICS_MODE");
+    else
+      printf("{ /*Bit16u ModeAttributes*/ %s,\n", 
+             "VBE_MODE_ATTRIBUTE_SUPPORTED | "
+             "VBE_MODE_ATTRIBUTE_EXTENDED_INFORMATION_AVAILABLE | "
+             "VBE_MODE_ATTRIBUTE_COLOR_MODE | "
+             "VBE_MODE_ATTRIBUTE_LINEAR_FRAME_BUFFER_MODE | "
+             "VBE_MODE_ATTRIBUTE_GRAPHICS_MODE");
+    printf("/*Bit8u  WinAAttributes*/ %s,\n",
+           "VBE_WINDOW_ATTRIBUTE_RELOCATABLE | "
+           "VBE_WINDOW_ATTRIBUTE_READABLE | "
+           "VBE_WINDOW_ATTRIBUTE_WRITEABLE");
 
-      printf("/*Bit8u  WinBAttributes*/ %d,\n", 0);
+    printf("/*Bit8u  WinBAttributes*/ %d,\n", 0);
 
-      printf("/*Bit16u WinGranularity*/ %s,\n", "VBE_DISPI_BANK_SIZE_KB");
+    printf("/*Bit16u WinGranularity*/ %s,\n", "VBE_DISPI_BANK_SIZE_KB");
 
-      printf("/*Bit16u WinSize*/ %s,\n", "VBE_DISPI_BANK_SIZE_KB");
+    printf("/*Bit16u WinSize*/ %s,\n", "VBE_DISPI_BANK_SIZE_KB");
 
-      printf("/*Bit16u WinASegment*/ %s,\n", "VGAMEM_GRAPH");
+    printf("/*Bit16u WinASegment*/ %s,\n", "VGAMEM_GRAPH");
 
-      printf("/*Bit16u WinBSegment*/ 0x%04x,\n", 0);
+    printf("/*Bit16u WinBSegment*/ 0x%04x,\n", 0);
 
-      printf("/*Bit32u WinFuncPtr*/ %d,\n", 0);
+    printf("/*Bit32u WinFuncPtr*/ %d,\n", 0);
 
-      printf("/*Bit16u BytesPerScanLine*/ %d,\n", pitch);
+    printf("/*Bit16u BytesPerScanLine*/ %d,\n", pitch);
 
-      // Mandatory information for VBE 1.2 and above
-      printf("/*Bit16u XResolution*/ %d,\n", pm->width);
-      printf("/*Bit16u YResolution*/ %d,\n", pm->height);
-      printf("/*Bit8u  XCharSize*/ %d,\n", 8);
-      printf("/*Bit8u  YCharSize*/ %d,\n", 16);
-      if (pm->depth == 4) {
-        printf("/*Bit8u  NumberOfPlanes*/ %d,\n", 4);
-      } else {
-        printf("/*Bit8u  NumberOfPlanes*/ %d,\n", 1);
-      }
-      printf("/*Bit8u  BitsPerPixel*/ %d,\n", pm->depth);
-      printf("/*Bit8u  NumberOfBanks*/ %d,\n", 
-             (pm->height * pitch + 65535) / 65536);
-
-      if (pm->depth == 4)
-        str = "VBE_MEMORYMODEL_PLANAR";
-      else if (pm->depth == 8)
-        str = "VBE_MEMORYMODEL_PACKED_PIXEL";
-      else
-        str = "VBE_MEMORYMODEL_DIRECT_COLOR";
-      printf("/*Bit8u  MemoryModel*/ %s,\n", str);
-      printf("/*Bit8u  BankSize*/ %d,\n", 0);
-      printf("/*Bit8u  NumberOfImagePages*/ %d,\n", 0);
-      printf("/*Bit8u  Reserved_page*/ %d,\n", 0);
-
-      // Direct Color fields (required for direct/6 and YUV/7 memory models)
-      switch(pm->depth) {
-        case 15:
-          r_size = 5;
-          r_pos = 10;
-          g_size = 5;
-          g_pos = 5;
-          b_size = 5;
-          b_pos = 0;
-          a_size = 1;
-          a_pos = 15;
-          break;
-        case 16:
-          r_size = 5;
-          r_pos = 11;
-          g_size = 6;
-          g_pos = 5;
-          b_size = 5;
-          b_pos = 0;
-          a_size = 0;
-          a_pos = 0;
-          break;
-        case 24:
-          r_size = 8;
-          r_pos = 16;
-          g_size = 8;
-          g_pos = 8;
-          b_size = 8;
-          b_pos = 0;
-          a_size = 0;
-          a_pos = 0;
-          break;
-        case 32:
-          r_size = 8;
-          r_pos = 16;
-          g_size = 8;
-          g_pos = 8;
-          b_size = 8;
-          b_pos = 0;
-          a_size = 8;
-          a_pos = 24;
-          break;
-        default:
-          r_size = 0;
-          r_pos = 0;
-          g_size = 0;
-          g_pos = 0;
-          b_size = 0;
-          b_pos = 0;
-          a_size = 0;
-          a_pos = 0;
-          break;
-      }
-
-      printf("/*Bit8u  RedMaskSize*/ %d,\n", r_size);               
-      printf("/*Bit8u  RedFieldPosition*/ %d,\n", r_pos);          
-      printf("/*Bit8u  GreenMaskSize*/ %d,\n", g_size);             
-      printf("/*Bit8u  GreenFieldPosition*/ %d,\n", g_pos);        
-      printf("/*Bit8u  BlueMaskSize*/ %d,\n", b_size);              
-      printf("/*Bit8u  BlueFieldPosition*/ %d,\n", b_pos);         
-      printf("/*Bit8u  RsvdMaskSize*/ %d,\n", a_size);              
-      printf("/*Bit8u  RsvdFieldPosition*/ %d,\n", a_pos);         
-      if (pm->depth == 32)
-        printf("/*Bit8u  DirectColorModeInfo*/ %s,\n",
-               "VBE_DIRECTCOLOR_RESERVED_BITS_AVAILABLE");
-      else
-        printf("/*Bit8u  DirectColorModeInfo*/ %s,\n", "0");
-
-      // Mandatory information for VBE 2.0 and above
-      printf("/*Bit32u PhysBasePtr*/ %s,\n", "0");
-      printf("/*Bit32u OffScreenMemOffset*/ %d,\n", 0);
-      printf("/*Bit16u OffScreenMemSize*/ %d,\n", 0);
-
-      // Mandatory information for VBE 3.0 and above
-      printf("/*Bit16u LinBytesPerScanLine*/ %d,\n", pitch);
-      printf("/*Bit8u  BnkNumberOfPages*/ %d,\n", 0);
-      printf("/*Bit8u  LinNumberOfPages*/ %d,\n", 0);
-      printf("/*Bit8u  LinRedMaskSize*/ %d,\n", r_size);
-      printf("/*Bit8u  LinRedFieldPosition*/ %d,\n", r_pos);
-      printf("/*Bit8u  LinGreenMaskSize*/ %d,\n", g_size);
-      printf("/*Bit8u  LinGreenFieldPosition*/ %d,\n", g_pos);
-      printf("/*Bit8u  LinBlueMaskSize*/ %d,\n", b_size);
-      printf("/*Bit8u  LinBlueFieldPosition*/ %d,\n", b_pos);
-      printf("/*Bit8u  LinRsvdMaskSize*/ %d,\n", a_size);
-      printf("/*Bit8u  LinRsvdFieldPosition*/ %d,\n", a_pos);
-      printf("/*Bit32u MaxPixelClock*/ %d,\n", 0);
-      printf("} },\n");
+    // Mandatory information for VBE 1.2 and above
+    printf("/*Bit16u XResolution*/ %d,\n", pm->width);
+    printf("/*Bit16u YResolution*/ %d,\n", pm->height);
+    printf("/*Bit8u  XCharSize*/ %d,\n", 8);
+    printf("/*Bit8u  YCharSize*/ %d,\n", 16);
+    if (pm->depth == 4) {
+      printf("/*Bit8u  NumberOfPlanes*/ %d,\n", 4);
+    } else {
+      printf("/*Bit8u  NumberOfPlanes*/ %d,\n", 1);
     }
+    printf("/*Bit8u  BitsPerPixel*/ %d,\n", pm->depth);
+    printf("/*Bit8u  NumberOfBanks*/ %d,\n", 
+           (pm->height * pitch + 65535) / 65536);
+    if (pm->depth == 4)
+      str = "VBE_MEMORYMODEL_PLANAR";
+    else if (pm->depth == 8)
+      str = "VBE_MEMORYMODEL_PACKED_PIXEL";
+    else
+      str = "VBE_MEMORYMODEL_DIRECT_COLOR";
+    printf("/*Bit8u  MemoryModel*/ %s,\n", str);
+    printf("/*Bit8u  BankSize*/ %d,\n", 0);
+    printf("/*Bit8u  NumberOfImagePages*/ %d,\n", 0);
+    printf("/*Bit8u  Reserved_page*/ %d,\n", 0);
+
+    // Direct Color fields (required for direct/6 and YUV/7 memory models)
+    switch(pm->depth) {
+      case 15:
+        r_size = 5;
+        r_pos = 10;
+        g_size = 5;
+        g_pos = 5;
+        b_size = 5;
+        b_pos = 0;
+        a_size = 1;
+        a_pos = 15;
+        break;
+      case 16:
+        r_size = 5;
+        r_pos = 11;
+        g_size = 6;
+        g_pos = 5;
+        b_size = 5;
+        b_pos = 0;
+        a_size = 0;
+        a_pos = 0;
+        break;
+      case 24:
+        r_size = 8;
+        r_pos = 16;
+        g_size = 8;
+        g_pos = 8;
+        b_size = 8;
+        b_pos = 0;
+        a_size = 0;
+        a_pos = 0;
+        break;
+      case 32:
+        r_size = 8;
+        r_pos = 16;
+        g_size = 8;
+        g_pos = 8;
+        b_size = 8;
+        b_pos = 0;
+        a_size = 8;
+        a_pos = 24;
+        break;
+      default:
+        r_size = 0;
+        r_pos = 0;
+        g_size = 0;
+        g_pos = 0;
+        b_size = 0;
+        b_pos = 0;
+        a_size = 0;
+        a_pos = 0;
+        break;
+    }
+
+    printf("/*Bit8u  RedMaskSize*/ %d,\n", r_size);               
+    printf("/*Bit8u  RedFieldPosition*/ %d,\n", r_pos);          
+    printf("/*Bit8u  GreenMaskSize*/ %d,\n", g_size);             
+    printf("/*Bit8u  GreenFieldPosition*/ %d,\n", g_pos);        
+    printf("/*Bit8u  BlueMaskSize*/ %d,\n", b_size);              
+    printf("/*Bit8u  BlueFieldPosition*/ %d,\n", b_pos);         
+    printf("/*Bit8u  RsvdMaskSize*/ %d,\n", a_size);              
+    printf("/*Bit8u  RsvdFieldPosition*/ %d,\n", a_pos);         
+    if (pm->depth == 32)
+      printf("/*Bit8u  DirectColorModeInfo*/ %s,\n",
+             "VBE_DIRECTCOLOR_RESERVED_BITS_AVAILABLE");
+    else
+      printf("/*Bit8u  DirectColorModeInfo*/ %s,\n", "0");
+     // Mandatory information for VBE 2.0 and above
+    printf("/*Bit32u PhysBasePtr*/ %s,\n", "0");
+    printf("/*Bit32u OffScreenMemOffset*/ %d,\n", 0);
+    printf("/*Bit16u OffScreenMemSize*/ %d,\n", 0);
+    // Mandatory information for VBE 3.0 and above
+    printf("/*Bit16u LinBytesPerScanLine*/ %d,\n", pitch);
+    printf("/*Bit8u  BnkNumberOfPages*/ %d,\n", 0);
+    printf("/*Bit8u  LinNumberOfPages*/ %d,\n", 0);
+    printf("/*Bit8u  LinRedMaskSize*/ %d,\n", r_size);
+    printf("/*Bit8u  LinRedFieldPosition*/ %d,\n", r_pos);
+    printf("/*Bit8u  LinGreenMaskSize*/ %d,\n", g_size);
+    printf("/*Bit8u  LinGreenFieldPosition*/ %d,\n", g_pos);
+    printf("/*Bit8u  LinBlueMaskSize*/ %d,\n", b_size);
+    printf("/*Bit8u  LinBlueFieldPosition*/ %d,\n", b_pos);
+    printf("/*Bit8u  LinRsvdMaskSize*/ %d,\n", a_size);
+    printf("/*Bit8u  LinRsvdFieldPosition*/ %d,\n", a_pos);
+    printf("/*Bit32u MaxPixelClock*/ %d,\n", 0);
+    printf("} },\n");
+  }
 
   printf("{ VBE_VESA_MODE_END_OF_LIST,\n");
   printf("{ 0,\n");
